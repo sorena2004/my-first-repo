48ef6929354b1d088886746cc57a6608b7a56025
sorena <hayesedward36@gmail.com>
2025-11-04 15:26:44 +0330

    change codes

--- test_calculator.py
+++ test_calculator.py
@@ -20,7 +20,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(divide(10, 2), 5)
     
     def test_divide_by_zero(self):
-        self.assertRaises(ValueError(divide(10, 0), error))
+        self.assertEqual(divide(10, 0), error)
 
 if __name__ == '__main__':
     unittest.main()
